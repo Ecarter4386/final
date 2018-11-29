016aed8f4e4b0ef9d47c66c4172c5c6770ab6057
ERIN CARTER <carter.erin86@gmail.com>
2018-11-29 18:54:08 -0500

    added css, links, and new html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < ActionController::Base
 	before_action :categories
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	helper_method :current_order
+
+	rescue_from CanCan::AccessDenied do |exception|
+		respond_to do |format|
+			format.json { head :forbidden}
+			format.html { redirect_to products_path, :alert => "Not authorized!"}
+				end
+			end
+
 	def categories
 		@categories = Category.order(:name)
 	end
 
+	def current_order
+		if !session[:order_id].nil?
+			Order.find(session[:order_id])
+		else
+			Order.new
+		end
+	end
+
+	
 	protected
 
 	def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   post 'add_to_cart' => 'cart#add_to_cart'
-
+  delete 'remove_from_cart' => 'cart#remove_from_cart'
   get 'view_order' => 'cart#view_order'
-
+  put 'view_order' => 'cart#view_order'
   get 'checkout' => 'cart#checkout'
+  post 'order_complete' => 'cart#order_complete'
+  put 'edit_order' => 'cart#edit_order'
 
   devise_for :users
+  
   root 'storefront#all_items'
   get 'categorical' => 'storefront#items_by_category'
+  get 'contact' => 'storefront#contact'
+  get 'follow' => 'storefront#follow'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
